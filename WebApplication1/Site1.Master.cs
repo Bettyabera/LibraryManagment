80d7a57cd7f054843139e0d9c2c1ecbc1e5e6bd3
Bettyabera <bettyabe@uw.edu>
2023-05-30 13:10:19 -0700

    change

--- WebApplication1/Site1.Master.cs
+++ WebApplication1/Site1.Master.cs
@@ -13,5 +13,40 @@ namespace WebApplication1
         {
 
         }
+
+        protected void LinkButton6_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("adminlogin.aspx");
+        }
+
+        protected void LinkButton11_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("author.aspx");
+        }
+
+        protected void LinkButton12_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("publisher.aspx");
+        }
+
+        protected void LinkButton8_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("bookinventory.aspx");
+        }
+
+        protected void LinkButton9_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("issuebook.aspx");
+        }
+
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("userlogin.aspx");
+        }
+
+        protected void LinkButton2_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("signUp.aspx");
+        }
     }
 }
